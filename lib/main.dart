2442c971ffa7fcf35efb1a2ef15379963db6979f
Gidzzy <gideonvertox@gmail.com>
2023-01-24 15:13:13 +0300

    added firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/colors.dart';
+import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/screens/mobile_layout_screen.dart';
 import 'package:whatsapp/screens/web_layout_screen.dart';
 import 'package:whatsapp/utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform );
+
   runApp(const MyApp());
 }
-
+  
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,4 +32,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
